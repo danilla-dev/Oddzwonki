57c7dc0e5e2c37b26b3907c43e7454b9f2477ab4
DanielKam <dkaminski38p@protonmail.com>
2023-05-01 21:31:46 +0200

    Delete src folder from repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-	entry: './src/js/app.js', // plik źródłowy, od którego zaczyna się budowanie
+	entry: './source/js/app.js', // plik źródłowy, od którego zaczyna się budowanie
 	output: {
 		filename: 'bundle.js', // nazwa pliku wyjściowego
 		path: path.resolve(__dirname, 'dist'), // ścieżka do katalogu, gdzie ma być zapisany plik wyjściowy
