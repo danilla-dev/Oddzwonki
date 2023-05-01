5f74e9dce4b87aef80865ae282025aea8bbdedef
DanielKam <dkaminski38p@protonmail.com>
2023-05-01 21:16:19 +0200

    Delete js files from repo

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import {} from './createCallbacks.js'
-import {} from './filterCallbacks.js'
-import {} from './handleCopyInfo.js'
-import {} from './handleDeleteCallback.js'
-import {} from './handleCounter.js'
-import {} from './handleAPI.js'
-import {} from './goUpBtn.js'
--- src/js/goUpBtn.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('.go-up-btn')
-
-btn.addEventListener('click', () => {
-	window.scrollTo(0, 0)
-})
