29ff085e5155fa6fd99a995b77b887ae23e25322
danilla-dev <dkaminski38p@protonmail.com>
2023-06-15 14:07:06 +0200

    New styles of buttons copy and edit, fixed bugs with copy

--- source/js/handleCopyInfo.js
+++ source/js/handleCopyInfo.js
@@ -1,15 +1,23 @@
 const container = document.querySelector('.callbacks-container')
 
 container.addEventListener('click', e => {
-	if (e.target.className === 'info-container__copy-btn') {
-		const info = e.target.parentElement.previousElementSibling.innerText
+	if (e.target.className === 'info-container__copy-btn' || e.target.className === 'copy-icon') {
+		console.log(e.target)
+		const info = e.target.closest('.info-container').firstElementChild.innerText
+		console.log(info)
 		navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
 			if (result.state == 'granted' || result.state == 'prompt') {
 			}
 		})
 		navigator.clipboard.writeText(info).then(() => {
-			e.target.innerText = 'Skopiowano'
-			e.target.style.opacity = '1'
+			if (e.target.tagName === 'BUTTON') {
+				// zrobic to za pomoca klasy a nie styli
+				e.target.style.backgroundColor = 'greenyellow'
+				e.target.style.opacity = '1'
+			} else {
+				e.target.parentElement.style.backgroundColor = 'greenyellow'
+				e.target.parentElement.style.opacity = '1'
+			}
 		})
 	}
 })
